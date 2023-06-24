dc2e0b69e308895c425d7d8132713295765a17f0
ApoTheddy <apotheddy@hotmail.com>
2023-06-23 22:06:02 -0500

    Commit Inicial - Agregando las carpetas y La conexion con SQL SERVER

---
+++ GDProyectoFinal/db/SqlDatabaseConnection.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace GDProyectoFinal.db
+{
+    public class SqlDatabaseConnection
+    {
+
+        public static SqlConnection GetConnecion()
+        {
+            const string url = "Data Source=DESKTOP-4HE0THI\\SQLEXPRESS;Integrated Security=True; database = ;";
+            try
+            {
+                using SqlConnection connection = new(url);
+                connection.Open();
+                return connection;
+            }
+            catch (Exception ex)
+            {
+                if (ex is SqlException sqlex)
+                    throw new Exception($"Ocurrio un error al conectar a SQL: {sqlex.Message}");
+                else
+                    throw new Exception($"Ocurrio un error al obtener la conexion: {ex.Message}");
+            }
+        }
+    }
+}
